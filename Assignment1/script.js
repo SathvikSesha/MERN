4568287c25167124d8bcab58b6a437805703b179
SathvikSesha <lakshmisarmatk@gmail.com>
2025-03-08 12:38:59 +0530

    Added link

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -3,18 +3,22 @@ document.addEventListener("DOMContentLoaded", () => {
   const select = document.querySelector(".select");
   const selected = document.querySelector(".selected");
   const options = document.querySelectorAll(".menu li");
-
   select.addEventListener("click", () => {
     dropdown.classList.toggle("active");
   });
-
   options.forEach((option) => {
-    option.addEventListener("click", () => {
-      selected.innerHTML = option.innerHTML;
+    option.addEventListener("click", (event) => {
+      event.stopPropagation();
+      const icon = option.querySelector("i").outerHTML;
+      const link = option.querySelector("a");
+      if (link) {
+        const linkText = link.innerText;
+        const href = link.href;
+        selected.innerHTML = `${icon} <a href="${href}" target="_blank">${linkText}</a>`;
+      }
       dropdown.classList.remove("active");
     });
   });
-
   document.addEventListener("click", (event) => {
     if (!dropdown.contains(event.target)) {
       dropdown.classList.remove("active");
